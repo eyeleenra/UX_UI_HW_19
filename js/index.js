f9efbced549457669ddf22199aa8430b3d485659
eyeleenra <raaileen113@gmail.com>
2024-08-02 23:24:07 -0700

    Edit

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('.scroll-down-arrow img').click(function() {
         $('html, body').animate({
             scrollTop: $('#about').offset().top
-        }, 1000); // Adjust the duration (in milliseconds) as needed
+        }, 1000);
     });
 });
 
