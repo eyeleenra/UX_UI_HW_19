39eb01b3bdf19ea56ffaff2bc30ed840250a6a1a
eyeleenra <raaileen113@gmail.com>
2024-08-02 21:46:17 -0700

    Update

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('JavaScript file loaded correctly.');
+
+// Get the button element by its ID
+const button = document.getElementById('testButton');
+
+// Add a click event listener to the button
+button.addEventListener('click', function() {
+    console.log('Button was clicked!');
+});
